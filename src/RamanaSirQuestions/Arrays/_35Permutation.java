a57707047360fa37381400ab89725e24b6deb1e5
SoumyakRanjanSahoo <soumyakranjan@gmail.com>
2025-08-30 18:24:23 +0530

    Permutation question solved..

--- src/RamanaSirQuestions/Arrays/_35Permutation.java
+++ src/RamanaSirQuestions/Arrays/_35Permutation.java
@@ -3,6 +3,24 @@ package RamanaSirQuestions.Arrays;
 public class _35Permutation {
     public static void main(String[] args) {
         String s="abc";
+        isPermutation(s,0,s.length());
+    }
 
+    private static void isPermutation(String s, int st, int end) {
+        if (st==end){
+            System.out.println(s);
+            return;
+        }
+        for (int i=st;i<end;i++){
+            String s1=swap(s,st,i);
+            isPermutation(s1,st+1,end);
+        }
+    }
+    private static String swap(String s,int i,int j){
+        char[]ch=s.toCharArray();
+        char temp=ch[i];
+        ch[i]=ch[j];
+        ch[j]=temp;
+        return new String(ch);
     }
 }
